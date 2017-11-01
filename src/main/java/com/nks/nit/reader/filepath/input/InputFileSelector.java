268ab60700620c55bc0e649d8cc084980a8dbad8
nirmal <nirmal@nirmal-Inspiron-5547>
2017-11-01 15:19:48 +0530

    comment removal

--- src/main/java/com/nks/nit/reader/filepath/input/InputFileSelector.java
+++ src/main/java/com/nks/nit/reader/filepath/input/InputFileSelector.java
@@ -6,11 +6,8 @@ import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-/* Class to to read a source file */
 public class InputFileSelector
 {
-	
-	/*A utility function to select and open the source file*/
 	public String getFilePath()
 	{
 		JFileChooser fileChooser=new JFileChooser(new File("/media/nirmal/GODOWN/QR"));
--- src/main/java/com/nks/nit/reader/filepath/output/DestinationPathSelector.java
+++ src/main/java/com/nks/nit/reader/filepath/output/DestinationPathSelector.java
@@ -4,10 +4,8 @@ import java.io.File;
 
 import javax.swing.JFileChooser;
 
-/* Class to save the file */
-public class DestinationPathSelector {
-
-	/* A utility function to get destination pathname and filename to save */
+public class DestinationPathSelector
+{
 	public String getDestinationPath()
 	{
 		String destinationPath = null;
